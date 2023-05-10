4c38b8b1fb46a89fcdcb436702fb74574370cabb
VladKk <mrvlad008@gmail.com>
2023-05-10 03:22:27 +0300

    Remove config.h file

--- config.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define QML_IMPORT_PATH "./qml"
-#define PLUGINS_OUTPUT_PATH "/home/vkoliadenko/projects/OfficeIS/build/plugins"
-
-#define OFFICEIS_NS OfficeIS
-#define OFFICEIS_NS_BEGIN namespace OFFICEIS_NS {
-#define OFFICEIS_NS_END }
